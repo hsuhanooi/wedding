76200303643e3d541f7a79ae0d8bbbd07c67e874
Hsu Han Ooi <hsuo@allenai.org>
2017-03-05 16:05:12 -0800

    add logo and views
    Former-commit-id: 7af5832665d1603ba150ef71c20c55e645eaae8b

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, browserHistory, Link} from 'react-router';
 
 import Index from './views/Index';
 import When from './views/When';
+import Registry from './views/Registry';
+import Rsvp from './views/Rsvp';
 
 import LogoImg from './images/2017-01-24_23-54-51_000.jpeg';
 
@@ -14,13 +16,15 @@ function MainLayout({ children }) {
       <div className="App">
         <div className="App-header">
           <div className="logo float-left">
-            <span className="logo">TINA AND HSU HAN</span>
+            <Link to='/' className="logo">
+              <span className="logo">TINA AND HSU HAN</span>
+            </Link>
           </div>
           <ul className="nav nav-pills float-right">
-            <li role="presentation"><a href="#">OUR STORY</a></li>
-            <li role="presentation"><a href="#">WHEN & WHERE</a></li>
-            <li role="presentation"><a href="#">REGISTRY</a></li>
-            <li role="presentation"><a href="#">RSVP</a></li>
+            <li role="presentation"><Link to='/'>OUR STORY</Link></li>
+            <li role="presentation"><Link to='/when'>WHEN & WHERE</Link></li>
+            <li role="presentation"><Link to='/registry'>REGISTRY</Link></li>
+            <li role="presentation"><Link to='/rsvp'>RSVP</Link></li>
           </ul>
         </div>
         {children}
@@ -32,6 +36,9 @@ const Root = ({store}) => (
   <Router history={browserHistory}>
     <Route component={MainLayout}>
         <Route path="/" component={Index} />
+        <Route path="/when" component={When} />
+        <Route path="/registry" component={Registry} />
+        <Route path="/rsvp" component={Rsvp} />
     </Route>
   </Router>
 );
