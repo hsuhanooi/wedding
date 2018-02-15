978493b24b33041130c4d2b09b2086713a4cc5a2
Hsu Han Ooi <hsuo@allenai.org>
2018-02-15 15:28:34 -0800

    use all cpus

--- src/express.js
+++ src/express.js
@@ -1,21 +1,34 @@
-const express = require('express')
-const app = express()
-const path = require('path');
+const cluster = require('cluster')
 
+if (cluster.isMaster) {
 
-// Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
 
-// Always return the main index.html, so react-router render the route in the client
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
-});
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+} else {
 
-module.exports = app;
+    const express = require('express')
+    const app = express()
+    const path = require('path')
 
-const PORT = process.env.PORT || 9000;
-console.log(PORT)
+    // Serve static assets
+    app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-});
+    // Always return the main index.html, so react-router render the route in the client
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+    });
+
+    module.exports = app;
+
+    const PORT = process.env.PORT || 9000;
+    console.log(PORT)
+
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}!`);
+    });
+}
